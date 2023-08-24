c273b202b67b92775b08b10f1875c909b46da979
Rudy Amri <rudyamri@Rudys-iMac.local>
2023-08-24 22:35:03 +0800

    refractor cart page

---
+++ src/app/components/cart-item/cart-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item',
+  templateUrl: './cart-item.component.html',
+  styleUrls: ['./cart-item.component.scss'],
+})
+export class CartItemComponent  implements OnInit {
+
+  @Input() item: any;
+  @Input() index: any;
+  @Output() add: EventEmitter<any> = new EventEmitter();
+  @Output() minus: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  onQuantityMinus() {
+    this.minus.emit(this.index)
+  }
+
+  onQuantityPlus() {
+    this.add.emit(this.index);
+  }
+
+}
