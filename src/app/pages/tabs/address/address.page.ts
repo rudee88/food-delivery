fcb08f10bc82d92becc43626b6a5a29adbf55468
Rudy Amri <rudyamri@Rudys-iMac.local>
2023-09-21 19:48:07 +0800

    refactor address page

--- src/app/pages/tabs/address/address.page.ts
+++ src/app/pages/tabs/address/address.page.ts
@@ -7,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressPage implements OnInit {
 
+  isLoading: boolean;
+  addresses: any[] = []
+
   constructor() { }
 
   ngOnInit() {
+  this.getAddresess();
   }
 
-  onEditAddress() {}
+  getAddresess() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.addresses = [
+      {address: 'Bandar Baru Ampang, Selangor', house: "30A, 1st Floor", id: "djkfda73k2", landmark: 'Ampang', lat: '3.144665133429708', lng: '101.7688121380485', title: 'Work', user_id: '1'},
+      {address: 'Jalan Gombak, Kuala Lumpur', house: "59-211-A", id: "kjadjds341", landmark: 'Batu 4', lat: '3.203287561584573', lng: '101.70513540425516', title: 'Home', user_id: '1'},
+    ];
+    this.isLoading = false;
+    },1000);
+  }
 
-  onDeleteAddress() {}
+  onGetIcon(title) {
+    const name = title.toLowerCase();
+    switch(name) {
+      case 'home': return 'home-outline';
+      case 'work': return 'briefcase-outline'
+      default: return 'location-outline';
+    }
+  }
+
+  onEditAddress(address) {}
+
+  onDeleteAddress(address) {}
 
 }
