3f36b03aacc11808b6087858ac4599da16b1f716
Rudy Amri <rudyamri@Rudys-iMac.local>
2023-09-26 19:28:22 +0800

    create storage service, update cart service/page

--- src/app/pages/tabs/cart/cart.page.ts
+++ src/app/pages/tabs/cart/cart.page.ts
@@ -4,6 +4,7 @@ import { Preferences } from '@capacitor/preferences';
 import { IonContent, NavController } from '@ionic/angular';
 import * as moment from 'moment';
 
+import { CartService } from './../../../services/cart/cart.service';
 import { OrderService } from '../../../services/order/order.service';
 import { GlobalService } from './../../../services/global/global.service';
 
@@ -26,7 +27,8 @@ export class CartPage implements OnInit {
     private router: Router, 
     private orderService: OrderService,
     private navCtrl: NavController,
-    private globalService: GlobalService
+    private globalService: GlobalService,
+    private cartService: CartService
     ) { }
 
   ngOnInit() {
@@ -34,49 +36,14 @@ export class CartPage implements OnInit {
     this.getModel();
   }
 
-  getCart() {
-    return Preferences.get({ key: 'cart' });
-  }
-
   async getModel() {
-    let data: any = await this.getCart();
     this.location = {
       lat: 3.143043190411341, 
       lng: 101.76289370565321,
       address: 'Ampang, Selangor'
     }
-    if (data?.value) {
-      this.model = await JSON.parse(data.value);
-      console.log('data: ', this.model);
-      this.calculate();
-    }
   }
 
-  async calculate() {
-    let item = this.model.items.filter(x => x.quantity > 0);
-    this.model.items = item;
-    this.model.totalPrice = 0;
-    this.model.totalItem = 0;
-    this.model.deliveryCharge = 0;
-    this.model.grandTotal = 0;
-
-    item.forEach((element) => {
-      this.model.totalItem += element.quantity;
-      this.model.totalPrice +=
-        parseFloat(element.price) * parseFloat(element.quantity);
-    });
-    this.model.deliveryCharge = this.deliveryCharge;
-    this.model.totalPrice = parseFloat(this.model.totalPrice).toFixed(2);
-    this.model.grandTotal = (parseFloat(this.model.totalPrice) + parseFloat(this.model.deliveryCharge)).toFixed(2);
-    if (this.model.totalItem == 0) {
-      this.model.totalItem = 0;
-      this.model.totalPrice = 0;
-      this.model.grandTotal = 0;
-      await this.clearCart();
-      this.model = null;
-    }
-    console.log('cart: ', this.model);
-  }
 
   clearCart() {
     return Preferences.remove({key: 'cart'});
@@ -98,25 +65,11 @@ export class CartPage implements OnInit {
   }}
 
   onQuantityMinus(index) {
-    if (this.model.items[index].quantity !== 0) {
-      this.model.items[index].quantity -= 1; // this.model.items[index].quantity = this.model.items[index].quantity - 1
-    } else {
-      this.model.items[index].quantity = 0;
-    }
-    this.calculate();
+    this.cartService.quantityMinus(index);
   }
  
   onQuantityPlus(index) {
-    try {
-      console.log(this.model.items[index]);
-      if (!this.model.items[index] || this.model.items[index].quantity == 0) {
-        this.model.items[index].quantity = 1;
-        this.calculate();
-      } else {
-        this.model.items[index].quantity += 1; // this.items[index].quantity = this.items[index].quantity + 1
-        this.calculate();
-      }
-    } catch (e) {}
+    this.cartService.quantityPlus(index);
   }
 
   onAddAddress() {
--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,9 +1,83 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  model: any = {};
+  deliveryCharge = 20;
+  private _cart = new BehaviorSubject<any>(null);
+
+  get cart() {
+    return this._cart.asObservable();
+  }
 
   constructor() { }
+
+  getCart() {
+    return Preferences.get({ key: 'cart' });
+  }
+
+  async getCartData() {
+    let data: any = await this.getCart();
+    if (data?.value) {
+      this.model = await JSON.parse(data.value);
+      console.log('data: ', this.model);
+      this.calculate();
+    }
+  }
+
+  quantityPlus(index) {
+    try {
+      console.log(this.model.items[index]);
+      if (!this.model.items[index] || this.model.items[index].quantity == 0) {
+        this.model.items[index].quantity = 1;
+      } else {
+        this.model.items[index].quantity += 1; // this.items[index].quantity = this.items[index].quantity + 1
+      }
+      this.calculate();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  quantityMinus(index) {
+    if (this.model.items[index].quantity !== 0) {
+      this.model.items[index].quantity -= 1; // this.model.items[index].quantity = this.model.items[index].quantity - 1
+    } else {
+      this.model.items[index].quantity = 0;
+    }
+  }
+
+  async calculate() {
+    let item = this.model.items.filter(x => x.quantity > 0);
+    this.model.items = item;
+    this.model.totalPrice = 0;
+    this.model.totalItem = 0;
+    this.model.deliveryCharge = 0;
+    this.model.grandTotal = 0;
+
+    item.forEach((element) => {
+      this.model.totalItem += element.quantity;
+      this.model.totalPrice +=
+        parseFloat(element.price) * parseFloat(element.quantity);
+    });
+    this.model.deliveryCharge = this.deliveryCharge;
+    this.model.totalPrice = parseFloat(this.model.totalPrice).toFixed(2);
+    this.model.grandTotal = (parseFloat(this.model.totalPrice) + parseFloat(this.model.deliveryCharge)).toFixed(2);
+    if (this.model.totalItem == 0) {
+      this.model.totalItem = 0;
+      this.model.totalPrice = 0;
+      this.model.grandTotal = 0;
+      await this.clearCart();
+      this.model = null;
+    }
+    console.log('cart: ', this.model);
+  }
+
+  clearCart() {
+
+  }
+ 
 }
