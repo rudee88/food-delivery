1004a40271d3651346a271ae18e0c6a98171fd89
Rudy Amri <rudyamri@Rudys-iMac.local>
2023-10-09 19:34:37 +0800

    edit address page created

---
+++ src/app/pages/tabs/address/edit-address/edit-address.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-address',
+  templateUrl: './edit-address.page.html',
+  styleUrls: ['./edit-address.page.scss'],
+})
+export class EditAddressPage implements OnInit {
+  form: FormGroup
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      title: new FormControl('', {validators: [Validators.required]}),
+      house: new FormControl('', {validators: [Validators.required]}),
+      landmark: new FormControl('', {validators: [Validators.required]}),
+    })
+  }
+
+}
