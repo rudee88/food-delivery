c29df149af1d4b8833f64ee98074b1c30f9628a8
rudy.amri <rudy.amri88@gmail.com>
2023-10-12 18:18:36 +0800

    add marker to map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -11,6 +11,7 @@ export class MapComponent  implements OnInit {
   @ViewChild('map', {static: true}) mapElementRef: ElementRef;
   googleMaps: any;
   map; any;
+  marker: any;
 
   constructor(
     private maps: GoogleMapsService,
@@ -26,8 +27,9 @@ export class MapComponent  implements OnInit {
       let googleMaps: any = await this.maps.loadGoogleMaps();
       this.googleMaps = googleMaps;
       const mapEl = this.mapElementRef.nativeElement;
+      const location = new googleMaps.LatLng(3.1448499743415494, 101.7689672668845);
       this.map = new googleMaps.Map(mapEl, {
-        center: new googleMaps.LatLng(3.1448499743415494, 101.7689672668845),
+        center: location,
         zoom: 15,
         scaleControl: false,
         streetViewControl: false,
@@ -51,9 +53,25 @@ export class MapComponent  implements OnInit {
       this.map.mapTypes.set('RSP Food Delivery', mapType);
       this.map.setMapTypeId('RSP Food Delivery');
       this.renderer.addClass(mapEl, 'visible');
+      this.addMaker(location);
     } catch(e) {
       console.log(e);
     }
+  }
+
+  addMaker(location) {
+    let googleMaps = this.googleMaps;
+    const icon = {
+      url: 'assets/icon/location-pin.png',
+      scaledSize: new googleMaps.Size(50, 50),
+    }
+    this.marker = new googleMaps.Marker({
+      position: location,
+      map: this.map,
+      icon: icon,
+      draggable: true,
+      animation: googleMaps.Animation.DROP
+    });
   }
 
 }
