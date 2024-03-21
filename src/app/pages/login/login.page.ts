78d4d9ecb715caf43e3b87c8aa580bfcac119d22
rudy.amri <rudy.amri88@gmail.com>
2024-03-22 00:46:19 +0800

    login page ui finish

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  // type = 'password';
+  // iconName = 'eye-outline';
+  type = true;
+  isLogin = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeType() {
+    // if (this.iconName === 'eye-outline') {
+    //   this.type = 'text';
+    //   this.iconName = 'eye-off-outline'
+    // } else {
+    //   this.type = 'password';
+    //   this.iconName = 'eye-outline'
+    // }
+    this.type = !this.type;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    if (!form.valid) return;
+    this.login(form);
+  }
+
+  login(form) {
+    this.isLogin = true;
   }
 
 }
