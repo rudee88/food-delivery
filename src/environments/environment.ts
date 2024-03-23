81fda06b322363e67e04f81fbff177a935bab83c
rudy.amri <rudy.amri88@gmail.com>
2024-03-23 23:10:24 +0800

    firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: "AIzaSyAOcT8u29OJO8qYkV7Fq00M4Bqveub77cc",
+    authDomain: "food-delivery-apps-f5c31.firebaseapp.com",
+    projectId: "food-delivery-apps-f5c31",
+    storageBucket: "food-delivery-apps-f5c31.appspot.com",
+    messagingSenderId: "29173354618",
+    appId: "1:29173354618:web:f587d52974a8222926abc3",
+    measurementId: "G-ZLCS3FES2R"
+  },
   googleMapsApikey: 'AIzaSyDK9oFbkDvH6w-FU9PoyTZKbkM937_sJ7I'
 };
 
