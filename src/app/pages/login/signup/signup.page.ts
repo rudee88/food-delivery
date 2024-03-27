0891cf795b8b0a0936441ce5e8f629dec06210b1
rudy.amri <rudy.amri88@gmail.com>
2024-03-28 00:32:19 +0800

    signup.ts updated

--- src/app/pages/login/signup/signup.page.ts
+++ src/app/pages/login/signup/signup.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { GlobalService } from 'src/app/services/global/global.service';
 
 @Component({
   selector: 'app-signup',
@@ -13,7 +14,8 @@ export class SignupPage implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private globalService: GlobalService
   ) { }
 
   ngOnInit() {
@@ -35,6 +37,11 @@ export class SignupPage implements OnInit {
     .catch(e => {
       console.log(e);
       this.isLoading = false;
+      let msg = 'Could not sign in you up, please try again';
+      if (e?.error?.message) {
+        msg = e.error.message;
+      }
+      this.globalService.showAlert(msg);
     });
   }
 
