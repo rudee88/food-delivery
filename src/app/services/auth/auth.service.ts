e3b15bbf06c47654951b9d1e6b16583be7450262
rudy.amri <rudy.amri88@gmail.com>
2024-03-28 23:42:50 +0800

    test push 2

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -58,7 +58,7 @@ export class AuthService {
 
   setUserData(token: string, user?) {
     // const data = {
-    //   email: user.email, test
+    //   email: user.email,
     //   type: user.type
     // };
     this.storage.setStorage('rsp_foodDelivery_token', token);
