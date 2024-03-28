addf3f40d83e9c41016f62b8b31ba954b570e17c
rudy.amri <rudy.amri88@gmail.com>
2024-03-29 00:33:17 +0800

    token interceptor added

--- src/app/services/token-interceptor/token-interceptor.service.ts
+++ src/app/services/token-interceptor/token-interceptor.service.ts
@@ -1,9 +1,33 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { StorageService } from '../storage/storage.service';
+import { AuthService } from '../auth/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService {
+export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // throw new Error('Method not implemented.');
+    const isApiUrl = req.url.startsWith(environment.serverBaseUrl)
+    return this.authService.isLoggedIn().pipe(
+      switchMap(token => {
+        console.log('token: ', token);
+        if (token && isApiUrl) {
+          req = req.clone({
+            setHeaders: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+        }
+        return next.handle(req);
+      })
+    );
+  }
 }
