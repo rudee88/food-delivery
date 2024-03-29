ca1a8e76be33ff716a9c60d11617165dc7d33403
rudy.amri <rudy.amri88@gmail.com>
2024-03-30 02:52:51 +0800

    otp input component created

---
+++ src/app/components/otp-input/otp-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-otp-input',
+  templateUrl: './otp-input.component.html',
+  styleUrls: ['./otp-input.component.scss'],
+})
+export class OtpInputComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
