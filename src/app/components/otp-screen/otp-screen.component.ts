8b2e8955edaa301922111888938354495ad7ff67
rudy.amri <rudy.amri88@gmail.com>
2024-04-05 05:31:23 +0800

    otp screen component created

---
+++ src/app/components/otp-screen/otp-screen.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-otp-screen',
+  templateUrl: './otp-screen.component.html',
+  styleUrls: ['./otp-screen.component.scss'],
+})
+export class OtpScreenComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
